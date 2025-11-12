97bcde39137540ea71b102d6e91fb42ccc67e0d0
shayaankhatri-hub <shayaan.khatri@merisehat.pk>
2025-11-12 15:33:24 +0500

    some care connect logic

--- pages/1_CareConnect.py
+++ pages/1_CareConnect.py
@@ -1,16 +1,33 @@
 import streamlit as st
 from utilities import test
 
-st.set_page_config(page_title="Apples", page_icon="🍎")
+#summary
+total_orders = 0
+return_to_origin = 0
+repeat_rate = 0
 
-st.header("Apples 🍎")
+#revenue/sales
+total_orders = total_orders
+repeat_purchase_rate = 0
+average_order_value = 0
 
-st.markdown('''
-Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus imperdiet lacus nulla, vitae faucibus erat cursus ut. Nullam quam lorem, semper eu nulla sit amet, pharetra viverra mi. Donec suscipit ligula metus, nec venenatis orci pellentesque et. Quisque ac sem eros. Duis non tellus vel est dictum interdum. Nam pulvinar mattis rhoncus. In sit amet ante ut odio scelerisque ullamcorper. Aliquam hendrerit facilisis purus eu mollis. Maecenas iaculis eget turpis nec mollis.
+#medical
+prescription_compliance_rate = 0
+top_prescribing_doctor = []
 
-Fusce ultrices tincidunt elit imperdiet dapibus. Nunc condimentum fermentum lacinia. Duis vestibulum neque posuere lorem tempus iaculis. Nulla ultrices accumsan est quis ullamcorper. Morbi convallis congue dui eu congue. Quisque laoreet turpis commodo luctus varius. Aliquam et iaculis erat. Mauris bibendum sem leo, id ultricies sapien viverra sed. Nunc id erat quis justo sagittis viverra ut id erat.
+#logistics
+on_time_delivery_rate = 0
+return_to_origin = 0
 
-Duis ultricies, sem eu fermentum fermentum, nisl tellus egestas odio, at placerat augue quam eget justo. Nullam facilisis porttitor ante, vitae blandit odio dapibus at. Nam tellus arcu, tristique sit amet posuere ut, posuere id nibh. Fusce ut diam dolor. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Nunc metus sapien, feugiat sed diam in, facilisis sollicitudin eros. Quisque quis ex et nunc pellentesque vestibulum. Praesent interdum molestie orci, id faucibus lorem blandit consequat. Vivamus aliquam, erat eu facilisis lacinia, felis nunc tristique lectus, iaculis pretium lectus massa eget risus. Pellentesque rhoncus arcu vel elementum venenatis. Ut in blandit libero, eget facilisis magna. Morbi aliquam elit ut neque imperdiet scelerisque.
-''')
+#geographic/source intelligence
+orders_by_city = []
+orders_by_source_channel = []
 
-test()
+#patient retention and satisfaction
+repeat_rate_for_delivered = 0
+
+#operations
+unfilled_orders = 0
+# --- Summary ---
+st.header("📊 KPI Summary — Executive Overview")
+
